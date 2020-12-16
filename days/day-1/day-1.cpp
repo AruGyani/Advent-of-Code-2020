14a93be0f0b5d0f3f6ca4ed007be463722f15015
Aru Gyani <aru@gyani.com>
2020-12-16 14:30:30 -0600

    Initial commit and day-1 solution

---
+++ days/day-1/day-1.cpp
@@ -0,0 +1,37 @@
+#include <fstream>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    ifstream inputFile;
+
+    inputFile.open("input.txt");
+
+    vector<int> arr;
+
+    if (inputFile) {
+        int num;
+        while (inputFile >> num) {
+            arr.push_back(num);
+        }
+    } else {
+        cout << "There was an error opening the input file." << endl;
+    }
+
+    for (int i = 0; i < arr.size(); i++) {
+        bool funcBreak = false;
+
+        for (int j = arr.size() - 1; j > 0; j--) {
+            if (arr.at(i) + arr.at(j) == 2020) {
+                cout << arr.at(i) * arr.at(j) << endl;
+                funcBreak = true;
+            }
+        }
+
+        if (funcBreak) break;
+    }
+
+    return 0;
+}
